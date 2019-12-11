1ba8df9b49b71e69465cbf5e644313226c5d8462
worepix <hry-kurasa@seznam.cz>
2019-12-11 19:07:23 +0100

    Add hanlders for volume and pairing new amplifier

--- app.py
+++ app.py
@@ -4,6 +4,31 @@ from flask import request as r
 import requests
 import jwt
 
+def text_response(text):
+    return {'fulfillmentText': text}
+
+def intent_handler_pair(request):
+    email = jwt.decode(request['originalDetectIntentRequest']['payload']['user']['idToken'], verify=False)['email']
+    id = request['queryResult']['parameters']['any']
+
+    l = requests.post('http://smart-amplifier-api.radimkozak.com/pair/new/amplifier', {'email': email, 'amplifier': id})
+
+    if l.status_code == 200:
+        return text_response('You amplifier with id {} was paired to your email'.format(id)), 200
+
+    return text_response('Error pairing amplifier with id {}. Make sure it is valid and registred amplifier'.format(id)), 200
+
+def intent_handler_volume(request):
+    volume = request['queryResult']['parameters']['percentage'][:-1]
+    email = jwt.decode(request['originalDetectIntentRequest']['payload']['user']['idToken'], verify=False)['email']
+
+    l = requests.post('http://smart-amplifier-api.radimkozak.com/change/volume/by/email', {'email': email, 'volume': volume})
+
+    if l.status_code == 200:
+        return text_response('Volume was changed to {} %'.format(volume)), 200
+
+    return text_response('Error while setting volume. Did you pair your amplifier. You can do it by saying or texting "Pair new amplifier <id>"'), 200
+
 def main():
     app = Flask(__name__)
 
@@ -11,21 +36,18 @@ def main():
     def route():
         try:
             request = r.get_json(silent=True)
-            volume = request['queryResult']['parameters']['percentage'][:-1]
-            email = jwt.decode(request['originalDetectIntentRequest']['payload']['user']['idToken'], verify=False)['email']
+            intent = request['queryResult']['intent']['displayName']
 
-            l = requests.post('http://smart-amplifier-api.radimkozak.com/change/volume/by/email', {'email': email, 'volume': volume})
+            if intent == 'Volume':
+                return intent_handler_volume(request)
 
-            if l.status_code == 200:
-                return {'fulfillmentText': 'Volume was changed to {} %'.format(volume)}, 200
-            
-            return {'fulfillmentText': 'Error while setting volume. Pair your account with amplifier'}, 200
+            if intent == 'Pair new amplifier':
+                return intent_handler_pair(request)
+
+            return text_response('Error while setting volume. Pair your account with amplifier'), 200
         except Exception:
-            return {'fulfillmentText': 'Error while setting volume'}, 200
-
-        print(volume)
-
-        return 'neco'
+            return text_response('Error while setting volume'), 200
+    
     app.run('0.0.0.0', '8080')
 
 if __name__ == '__main__':
